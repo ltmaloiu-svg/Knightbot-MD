29a946fcefbed9376cd07e25f596a748d9b6a874
ilmohamad161-cpu <ilmohamad161@gmail.com>
2026-01-04 13:12:58 +0100

    chore: update settings.js (update bot metadata and config)

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight Bot',
-  author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  packname: 'غريفيث',
+  author: 'ريو',
+  botName: "غريفيث",
+  botOwner: 'ريو', // Your name
+  ownerNumber: '212674751039', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
