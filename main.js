306f3ffaecc6dcd4d0abd69f0e5331e661e0e9b1
ltmaloiu-svg <ltmaloiu@gmail.com>
2026-01-05 15:44:10 +0100

    Update main.js

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ const { autoreadCommand, isAutoreadEnabled, handleAutoread } = require('./comman
 
 // Command imports
 const tagAllCommand = require('./commands/tagall');
-const helpCommand = require('./commands/مساعدة');
+const helpCommand = require('./commands/help');
 const banCommand = require('./commands/ban');
 const { promoteCommand } = require('./commands/promote');
 const { demoteCommand } = require('./commands/demote');
